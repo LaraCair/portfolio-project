567645ab9672586dd820ce6e124b244a4c1b0bd9
Lara <pptopol@yandex.ru>
2023-05-25 19:32:39 +0300

    проект персонаж и записная книжка

--- pages/projects/personage.js
+++ pages/projects/personage.js
@@ -1,16 +1,37 @@
 const button = document.querySelector('.getPersonageButton');
 const image = document.querySelector('.personagePicture');
-const info = document.querySelector('.personageinfo')
-button.addEventListener('click', getRandomPersonageinfo);
+const family= document.querySelector('.personageFamily');
+const fullName = document.querySelector('.personageFullName');
+const title = document.querySelector('.personageTitle');
+button.addEventListener('click',getRandomPersonageInfo);
 
-function getRandomPersonageImage() {
-   const resalt =  fetch('https://thronesapi.com//api/v2/Characters')
+function getRandomPersonageInfo() {
+   let randomPersonageImage = ''
+   let randomPersonageFamily = ''
+   let randomPersonageFullName = ''
+   let randomPersonageTitle = ''
+
+
+   const result =  fetch('https://thronesapi.com/api/v2/Characters')
+   .then(response => response.json())
+   .then(json => {
+       
+      const randomPersonageInfoIndex =Math.floor(Math.random()*json.length);
+
+      randomPersonageImage += json[randomPersonageInfoIndex].imageUrl;
+      image.setAttribute('src',randomPersonageImage);
+
+      randomPersonageFamily += json[randomPersonageInfoIndex].family;
+      family.innerHTML = randomPersonageFamily;
+
+      randomPersonageFullName += json[randomPersonageInfoIndex].fullName;
+      fullName.innerHTML =randomPersonageFullName;
+ 
+      randomPersonageTitle += json[randomPersonageInfoIndex].title;
+      title.innerHTML =randomPersonageTitle;
+
    
-    .then(response => response.json())
-    .then(json => {
-       image.setAttribute('src',json[0].imageURL)
-    });
-  
-}
+   });
+} 
 
-getRandomPersonageinfo();
+getRandomPersonageInfo();
--- pages/projects/time.js
+++ pages/projects/time.js
@@ -1,20 +1,44 @@
 const hoursElement = document.querySelector('.hours');
 const minutesElement = document.querySelector('.minutes');
 const secondsElement = document.querySelector('.seconds');
+const date = document.querySelector('.date.date')
 const clock = document.querySelector('.clock');
+const loader = document.querySelector('.loader');
+
+const monthMap = {
+    '0': 'января',
+    '1': 'февраля',
+    '2': 'марта',
+    '3': 'апреля',
+    '4': 'мая',
+    '5': 'июня',
+    '6': 'июля',
+    '7': 'августа',
+    '8': 'сентября',
+    '9': 'октября',
+    '10': 'ноября',
+    '11': 'декабря'
+       
+}    
 
 function validateTime(nun) {
     const correctNumber = nun < 10 ? '0'+ nun: nun;
     return correctNumber;
 }
 
-function initTimer(){
+function setTime(){
     const currentTime = new Date();
-
+    
+    date.innerHTML=`Сегодня ${currentTime.getDay()} ${monthMap[currentTime.getMonth()]} ${currentTime.getFullYear()}года `
     hoursElement.innerHTML = currentTime.getHours();
     minutesElement.innerHTML = currentTime.getMinutes();
     secondsElement.innerHTML = currentTime.getSeconds();
 }
+function showClock() {
+  loader.style.display= 'none';
+  clock.style.display= 'block';
+}
+
 
 setInterval(setTime, 1000);
-setTimeout
+setTimeout(showClock, 1000);
